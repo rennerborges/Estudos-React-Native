e3361f2f9c57b67d56315d3b377ed32f20d043b3
Renner Borges <rennerferreira23@gmail.com>
2020-01-13 17:11:45 -0300

    Conclusão FlatList
    Foi terminado o Drawer do FlatList

--- exercicios/src/components/ListaFlex/index.js
+++ exercicios/src/components/ListaFlex/index.js
@@ -11,23 +11,23 @@ const aluno = [
     { id: 7, nome: 'Marcelo', nota: 7.3 },
     { id: 8, nome: 'Marco', nota: 7.2 },
 
-    { id: 11, nome: 'Renner', nota: 7.9 },
-    { id: 12, nome: 'Wanderson', nota: 9 },
-    { id: 13, nome: 'Thander', nota: 7.5 },
-    { id: 14, nome: 'Halyson', nota: 8 },
-    { id: 15, nome: 'Luiz Alexandre', nota: 4 },
-    { id: 16, nome: 'Mateus', nota: 5 },
-    { id: 17, nome: 'Marcelo', nota: 7.3 },
-    { id: 18, nome: 'Marco', nota: 7.2 },
+    { id: 11, nome: 'Vitoria', nota: 7.9 },
+    { id: 12, nome: 'Bruno', nota: 9 },
+    { id: 13, nome: 'Bruna', nota: 7.5 },
+    { id: 14, nome: 'Gabriela', nota: 8 },
+    { id: 15, nome: 'Djoan', nota: 4 },
+    { id: 16, nome: 'Vitor Hugo', nota: 5 },
+    { id: 17, nome: 'Saulo', nota: 7.3 },
+    { id: 18, nome: 'Ana Clara', nota: 7.2 },
 
-    { id: 21, nome: 'Renner', nota: 7.9 },
-    { id: 22, nome: 'Wanderson', nota: 9 },
-    { id: 23, nome: 'Thander', nota: 7.5 },
-    { id: 24, nome: 'Halyson', nota: 8 },
-    { id: 25, nome: 'Luiz Alexandre', nota: 4 },
-    { id: 26, nome: 'Mateus', nota: 5 },
-    { id: 27, nome: 'Marcelo', nota: 7.3 },
-    { id: 28, nome: 'Marco', nota: 7.2 },
+    { id: 21, nome: 'Saulo', nota: 7.9 },
+    { id: 22, nome: 'Kelly', nota: 9 },
+    { id: 23, nome: 'Bernado', nota: 7.5 },
+    { id: 24, nome: 'Tulio', nota: 8 },
+    { id: 25, nome: 'Liz', nota: 4 },
+    { id: 26, nome: 'Tobias', nota: 5 },
+    { id: 27, nome: 'Livia', nota: 7.3 },
+    { id: 28, nome: 'Washington', nota: 7.2 },
 ];
 
 const itemEstilo = {
@@ -47,11 +47,14 @@ export const Aluno = props =>
     </View>
 
 export default props =>{
-    const renderItems = ({item}) => <Aluno {...item}/>
-
-    return (
-        <ScrollView>
-            <FlatList data={aluno} renderItem={renderItems} keyExtractor={(_,index)=> index.toString()}  />
-        </ScrollView>
+    const renderItem = ({item}) =>{
+        return <Aluno {...item}/>
+    }
+    return(
+        <View>
+            <ScrollView>
+                <FlatList data={aluno} renderItem={renderItem} keyExtractor={(_, index)=> index.toString()} />
+            </ScrollView>
+        </View>
     );
 }
